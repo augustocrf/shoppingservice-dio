e43d26b5b7da0f3b4b096baaff9b90073bdf5e96
augustocrf <augustocrf@gmail.com>
2021-08-29 17:00:24 -0300

    Developed ElasticSearch Config and README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# shoppingservice-dio" 
+# Project Shoppingservice Dio
+Construindo um projeto com arquittura baseada em microsserviços usando Spring Cloud
+
+Java | Spring Boot | Spring Cloud | Elastic Search   
--- product-catalog/src/main/java/br/com/acrf/shoppingservice/productcatalog/config/ElasticSearchConfig.java
+++ product-catalog/src/main/java/br/com/acrf/shoppingservice/productcatalog/config/ElasticSearchConfig.java
@@ -2,15 +2,19 @@ package br.com.acrf.shoppingservice.productcatalog.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "br.com.acrf.shoppingservice.productcatalog.service")
+@EnableElasticsearchRepositories(basePackages = "br.com.acrf.shoppingservice.productcatalog.repository")
+@ComponentScan(basePackages = { "br.com.acrf.shoppingservice.productcatalog.service" })
 public class ElasticSearchConfig  extends AbstractElasticsearchConfiguration {
 
     @Override
@@ -20,5 +24,10 @@ public class ElasticSearchConfig  extends AbstractElasticsearchConfiguration {
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
     }
 }
