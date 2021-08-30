063f60239bb3df4a0686df684ab80cf213a89ded
augustocrf <augustocrf@gmail.com>
2021-08-30 11:57:51 -0300

    Developed Service Discovery

---
+++ service-discovery/src/test/java/br/com/acrf/shoppingservice/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acrf.shoppingservice.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
